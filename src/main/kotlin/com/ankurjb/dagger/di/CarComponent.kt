bac6bf540958fcce8afb9c9148990d03c2fc381b
Ankur Singh <ankursinghjb@gmail.com>
2022-01-23 02:11:29 +0530

    method injection

--- src/main/kotlin/com/ankurjb/dagger/di/CarComponent.kt
+++ src/main/kotlin/com/ankurjb/dagger/di/CarComponent.kt
@@ -1,11 +1,9 @@
 package com.ankurjb.dagger.di
 
 import com.ankurjb.dagger.MyActivity
-import com.ankurjb.dagger.models.Car
 import dagger.Component
 
 @Component
-interface CarMyComponent {
-    fun provides(): Car
+interface CarComponent {
     fun inject(myActivity: MyActivity)
 }
--- src/main/kotlin/com/ankurjb/dagger/models/CarParts.kt
+++ src/main/kotlin/com/ankurjb/dagger/models/CarParts.kt
@@ -6,10 +6,28 @@ class Car @Inject constructor(
     private val engine: Engine,
     private val wheels: Wheels
 ) {
-    fun getCarName() {
-        println("lamborghini")
+    fun drive() {
         println(engine.getEngine())
         println(wheels.getWheels())
+    }
+
+    fun getCarName() = "lamborghini"
+
+
+    /**
+     * We need to add Inject on function because it requires a Remote object which needs to be injected
+     *
+     * Since constructor or Car is injected we don't need to manually inject remote using dagger
+     * by using DaggerComponent.create() like we did in our Activity
+     *
+     * Order will be like
+     * 1) constructor injection
+     * 2) field injection
+     * 3) method injection
+     */
+    @Inject
+    fun addRemote(remote: Remote) {
+        remote.attachRemote(this)
     }
 }
 
@@ -19,4 +37,10 @@ class Engine @Inject constructor() {
 
 class Wheels @Inject constructor() {
     fun getWheels() = "wheels rolling"
+}
+
+class Remote @Inject constructor() {
+    fun attachRemote(car: Car) {
+        println("remote added to ${car.getCarName()}")
+    }
 }
